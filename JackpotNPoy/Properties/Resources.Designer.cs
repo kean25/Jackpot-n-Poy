7b795418bc97ec6be98315feccc7a57f468b8308
White Flower <jfouryeah36@gmail.com>
2021-11-15 21:26:31 +0800

    Update
    Added some images

--- JackpotNPoy/Properties/Resources.Designer.cs
+++ JackpotNPoy/Properties/Resources.Designer.cs
@@ -69,5 +69,25 @@ namespace JackpotNPoy.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap DefaultNoUser {
+            get {
+                object obj = ResourceManager.GetObject("DefaultNoUser", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Userprofilepicture {
+            get {
+                object obj = ResourceManager.GetObject("Userprofilepicture", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
